368cafbe0c3052ac17397f629e67878aa36c20b8
Chris <csnow@live.ca>
2021-02-23 23:06:46 -0500

    Attempted fix for disconnections (again).

--- game/__init__.py
+++ game/__init__.py
@@ -3,12 +3,13 @@ a = Flask(__name__)
 
 from flask_socketio import SocketIO
 socketio = SocketIO(a,
+  cookie='gamesession',
   cors_credentials=True,
   cors_allowed_origins=[
     "http://localhost:5000",
     "https://scrolling-map.herokuapp.com"
   ],
-  logger=True, engineio_logger=True
+  # logger=True, engineio_logger=True
 )
 
 from flask_sqlalchemy import SQLAlchemy
